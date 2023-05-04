03cc0a2c16a81244f0cd80e982fd1f6d654c144d
Vadim <vadimgol07@inbox.ru>
2023-05-04 20:25:43 +0300

    test

--- pages/demoqa.py
+++ pages/demoqa.py
@@ -1,18 +1,31 @@
-from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoSuchElementException
 from pages.base_page import BasePage
+from components.components import WebElement
 
 class DemoQa(BasePage):
 
-    def exist_icon(self):
-        try:
-            self.find_element(locator='#app>header>a')
-        except NoSuchElementException:
-            return False
-        return True
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/'
+        super ().__init__(driver, self.base_url)
+
+        self.icon = WebElement(driver, '#app > header > a')
+        self.btn_elements = WebElement(driver, '#app > div > div > div.home-body > div > div:nth-child(1)')
+
+
     def click_on_the_icon(self):
         self.find_element(locator='#app>header>a').click()
 
+    def click_on_the_btn_elements(self):
+        self.find_element(locator= '#app > div > div > div.home-body > div > div:nth-child(1)').click()
+
     def equel_url(self):
-        if self.get_url() == 'https://demoqa.com/':
+        if self.get_url() == self.base_url:
             return True
         return False
+
+    # def exist_icon(self):
+    #  try:
+    #      self.icon.find_element() #locator='#app>header>a'
+    #  except NoSuchElementException:
+    #     return False
+    #   return True
